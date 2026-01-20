b4e0b4507ca48ed8cee3d5590271dbb4b7600917
Rebecca Shannon <rebshan@bu.edu>
2026-01-20 16:24:50 -0500

    qsub and log

--- qsub_bubbleTest_2112.sh
+++ qsub_bubbleTest_2112.sh
@@ -3,7 +3,7 @@
 #$ -m bea 
 #$ -j y
 #$ -N sembEllipse
-#$ -l h_rt=96:00:00
+#$ -l h_rt=12:00:00
 #MPI_BUFFER_SIZE = 8192
 #$ -P aeracous
 
@@ -16,9 +16,11 @@ module load python3
 conda activate work-env
 export PYTHONPATH='/projectnb/aeracous/REBECCA/Silo-main/install/lib64:'${PYTHONPATH}
 
+cd $AHOME/postProcess_drop_diameter/
+
 pip install -e .
 echo 'installed, running code now...'
-
+echo 'conda prefix is'$CONDA_PREFIX
 python of_getDiams.py
 
 echo 'after python code here'
